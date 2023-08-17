cf185ca62094a7eae134c4a3d2af914ea4bea79e
Mahad Afridi <mahadakbarafridi@gmail.com>
2023-08-16 19:46:41 -0700

    Built the network

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import imghdr
 import numpy as np
 import matplotlib.pyplot as plt
 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+
 #this is used to prevent out of memory errors
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus: #go through every gpus inside of it and run this on every one of them
@@ -62,3 +65,31 @@ data = data.map(lambda x,y:(x/255, y)) #x is the images, y is the labels
 train_size = int(len(data)* .7)
 val_size = int(len(data)*.2)
 test_size = int(len(data)*.1) * 1
+
+train = data.take(train_size)
+val = data.skip(train_size).take(val_size)
+test = data.skip(train_size+val_size).take(test_size)
+
+#model part
+
+model = Sequential()
+
+#adding a convolutional layer and a max pooling layer
+
+#parameters contain the number of filters, the size of the filter, and the stride
+model.add(Conv2D(16, (3,3), 1, activation='relu', input_shape=(256,256,3)))
+model.add(MaxPooling2D())
+
+model.add(Conv2D(32, (3,3), 1, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Conv2D(16, (3,3), 1, activation='relu'))
+model.add(MaxPooling2D())
+
+#flatten the data
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+
+#makes it into a single layer
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile('adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
